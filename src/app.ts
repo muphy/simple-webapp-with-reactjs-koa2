b465eb95c471da9b0ee7567639f0d4662595a38f
liamoh <liamoh@liamoh-MacBook-Pro.local>
2018-03-19 21:04:18 +0900

    Add basic web server
    실행시 Promise' only refers to a type, but is being used as a value here. 에러발생

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import * as Koa from "koa";
+import * as bodyParser from "koa-bodyparser";
+import * as logger from "koa-logger";
+import * as Router from "koa-router";
+import * as jwt from "koa-jwt";
+
+import { createConnection } from "typeorm";
+import { Inject } from "typescript-ioc";
+
+export default class App {
+
+    private async createApp() {
+
+        const app: Koa = new Koa();
+        const router: Router = new Router();
+
+        // Middleware below this line is only reached if JWT token is valid
+        // unless the URL starts with '/public'
+        //ctx.state.user
+        app.use(logger());
+        app.use(bodyParser());
+        app.use(router.allowedMethods());
+        app.use(jwt({ secret: "dnc" }).unless({ path: [/^\/api\/public/, /^\/public/] }));
+        app.use(router.routes());
+
+        return Promise.resolve(app);
+    }
+
+    public async start() {
+        const app = await this.createApp();
+        console.log("Started listening on port 3000...");
+        const server = app.listen(3000);
+        return Promise.resolve(server);
+    }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "reflect-metadata";
+
+import { Container } from "typescript-ioc";
+
+import App from "./app";
+
+const app: App = Container.get(App);
+
+app.start();

ae241a16ebfd813dba2e4a4eefcd0ee2a2e49d0a
liamoh <liamoh@liamoh-MacBook-Pro.local>
2018-03-20 23:43:53 +0900

    Add client code
    generate boilerplate code by create-react-app
    reference url: https://goo.gl/hXKkGf

--- src/app.ts
+++ src/app.ts
@@ -53,8 +53,8 @@ export default class App {
 
     public async start() {
         const app = await this.createApp();
-        console.log("Started listening on port 3000...");
-        const server = app.listen(3000);
+        console.log("Started listening on port 4000...");
+        const server = app.listen(4000);
         return Promise.resolve(server);
     }
 
